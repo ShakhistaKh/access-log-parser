e8498ecd045a398c39b535024dcdc4223c26296c
shokhista <shokhistakhashimova@gmail.com>
2025-02-26 20:25:36 +0300

    Math operations done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("6" + Math.random());
+//        System.out.println("Введите первое число:");
+//        System.out.println("Введите второе число:");
+//        Scanner input = new Scanner(System.in);
+//        int x = input.nextInt();
+//        int y = input.nextInt();
+
+
+       int firstNumber = 5;
+       int secondNumber = 10;
+       double quotient = (double) firstNumber/secondNumber;
+       String str1 = "Сума: ";
+       String str2 = "Разность: ";
+       String str3 = "Произведение: ";
+       String str4 = "Частное: ";
+       System.out.println(str1+firstNumber+secondNumber);
+       System.out.println(str2+(firstNumber-secondNumber));
+       System.out.println(str3+(firstNumber*secondNumber));
+       System.out.println(str4+((double)(firstNumber/secondNumber)));
     }
 }
